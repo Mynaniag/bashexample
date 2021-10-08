44bcab0d77c0b523d776db7e81976f9b79405fe4
Nikolay Doroshchenko <mynaniag@gmail.com>
2021-10-08 07:44:18 +0000

    Add two bash script

---
+++ first.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env
+set -x
+#this is my comment
+<<MULTILINE
+and
+write
+a few
+lines of a
+long
+comment
+MULTILINE
+
+echo Done
---
+++ second.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+touch /tmp/mytempfile
+ls /tmp
+mkdir /tmp/mytempdir
+ls /tmp/mytempdir
+mv /tmp/mytempfile /tmp/mytempdir/
+ls /tmp/mytempdir
+rm -r /tmp/mytempdir
+ls /tmp/
